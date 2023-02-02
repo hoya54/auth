246301c59b6d2f9999cf0ab508ff3dde635f4fba
hoya445 <hoya445@naver.com>
2023-02-02 21:39:55 +0900

    .

--- backend/auth/src/main/java/mpti/auth/api/controller/IndexController.java
+++ backend/auth/src/main/java/mpti/auth/api/controller/IndexController.java
@@ -1,13 +1,15 @@
 package mpti.auth.api.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = {"http://localhost:3000", "http://127.0.0.1:3000"})
 @RestController
 @RequestMapping("/ji")
 public class IndexController {
-    @GetMapping("")
+    @GetMapping("/sun")
     public String checkDuplicateId() {
         return "<h1>Hello Auth Server Main Page</h1>";
     }
