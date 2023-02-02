f5f8d67952942d202279580735ac5466682af659
hoya54 <62598865+hoya54@users.noreply.github.com>
2023-02-02 17:39:43 +0900

    Update IndexController.java

--- backend/auth/src/main/java/mpti/auth/api/controller/IndexController.java
+++ backend/auth/src/main/java/mpti/auth/api/controller/IndexController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/auth")
 public class IndexController {
     @GetMapping("")
     public String checkDuplicateId() {
